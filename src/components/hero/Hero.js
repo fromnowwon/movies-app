f82fb252cb8abb6e84c46077b8864f4034763100
cw <fromnowwon@gmail.com>
2023-07-29 21:05:20 +0900

    feat: create trailer component with react-player

--- src/components/hero/Hero.js
+++ src/components/hero/Hero.js
@@ -1,13 +1,16 @@
 import './Hero.css';
 import Carousel from 'react-material-ui-carousel';
 import { Paper } from '@mui/material';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCirclePlay } from '@fortawesome/free-solid-svg-icons';
+import { Link } from 'react-router-dom';
 
 const Hero = ({ movies }) => {
 	return (
 		<div className="movie-carousel-container">
 			<Carousel>
 				{
-					movies && movies.map((movie) => {
+					movies?.map((movie) => {
 						return (
 							<div key={movie.imdbId}>
 								<Paper>
@@ -19,6 +22,13 @@ const Hero = ({ movies }) => {
 												</div>
 												<div className="movie-title">
 													<h4>{movie.title}</h4>
+												</div>
+												<div className="movie-buttons-container">
+													<Link to={`/Trailer/${movie.trailerLink.substring(movie.trailerLink.length - 11)}`}>
+														<div className="play-button-icon-container">
+															<FontAwesomeIcon className='play-button-icon' icon={faCirclePlay} />
+														</div>
+													</Link>
 												</div>
 											</div>
 										</div>
