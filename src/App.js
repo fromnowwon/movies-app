3c37213ba8cc0ed5fecfb17ed2092843b3203280
cw <fromnowwon@gmail.com>
2023-07-30 23:34:27 +0900

    feat: create movie reviews functionality

--- src/App.js
+++ src/App.js
@@ -6,16 +6,30 @@ import Home from './components/home/Home';
 import { Route, Routes } from 'react-router-dom';
 import Header from './components/header/Header';
 import Trailer from './components/trailer/Trailer';
+import Reviews from './components/reviews/Reviews';
 
 function App() {
 	const [movies, setMovies] = useState();
+	const [movie, setMovie] = useState();
+	const [reviews, setReviews] = useState();
+
 	const getMovies = async () => {
 		try {
 			const response = await api.get("/api/v1/movies");
-			console.log(response.data);
 			setMovies(response.data);
 		} catch (err) {
 			console.log(err);
+		}
+	}
+
+	const getMovieData = async (movieId) => {
+		try {
+			const response = await api.get(`/api/v1/movies/${movieId}`);
+			const singleMovie = response.data;
+			setMovie(singleMovie);
+			setReviews(singleMovie.reviews);
+		} catch (err) {
+			console.error(err);
 		}
 	}
 
@@ -30,6 +44,7 @@ function App() {
 				<Route path="/" element={<Layout/>}>
 					<Route path="/" element={<Home movies={movies}/>}></Route>
 					<Route path="/Trailer/:ytTrailerId" element={<Trailer/>}></Route>
+					<Route path="/Reviews/:movieId" element={<Reviews getMovieData={getMovieData} movie={movie} reviews={reviews} setReviews={setReviews} />}></Route>
 				</Route>
 			</Routes>
     </div>
--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export default axios.create({
-	baseURL: 'https://c459-61-105-29-161.ngrok-free.app/',
+	baseURL: 'https://7051-61-105-29-161.ngrok-free.app/',
 	headers: {"ngrok-skip-browser-warning": true},
 });
